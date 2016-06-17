3949e41077a9bd26a6d5cf3cf732817918f5306e
Niklas Johansson <raphexion@gmail.com>
2016-06-17 12:44:39 +0200

    Simplest possible test of rule 4

--- src/test/scala/com/couch_red/abstractmachines/KMachineSuite.scala
+++ src/test/scala/com/couch_red/abstractmachines/KMachineSuite.scala
@@ -10,16 +10,24 @@ class KMachineSuite extends FunSuite {
   import com.couch_red.abstractmachines.KMachine._
 
   trait TestVars {
-    val identity = Abstraction(DeBruijn(0))
+    val deBruijn0 = DeBruijn(0)
+    val identity = Abstraction(deBruijn0)
     val minimal = Application(identity, identity)
   }
 
   test("Program on page 106 - Abstract Computing Machines") {
     new TestVars {
       val ts0 = new TransitionState(Nil, minimal::Nil, Nil)
+
       val ts1 = transfer(ts0)
       assert(ts1.environment == Nil)
+      assert(ts1.terms == identity::Nil)
       assert(ts1.stack == Suspension(Nil, identity::Nil) :: Nil)
+
+      val ts2 = transfer(ts1)
+      assert(ts2.environment == Suspension(Nil, identity::Nil)::Nil)
+      assert(ts2.terms == deBruijn0::Nil)
+      assert(ts2.stack == Nil)
     }
   }
 }
